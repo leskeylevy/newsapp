56facd13420955a1260dde92d6f28e2dc20870f3
Levy <leskeylevy@gmail.com>
2018-09-03 14:33:39 +0300

    moved the source class from source.py to model.py

---
+++ app/model.py
@@ -0,0 +1,10 @@
+class Source:
+    '''
+    source clss definiton
+    '''
+
+    def __init__(self, name, url, description, rt):
+        self.name = name
+        self.url = url
+        self.description = description
+        self.rt = rt

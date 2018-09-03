36b9e4b1369b055fa9ddec579f0c90c380d29961
Levy <leskeylevy@gmail.com>
2018-09-03 14:35:38 +0300

    moved the articles class from articles.py to model.py

--- app/model.py
+++ app/model.py
@@ -8,3 +8,25 @@ class Source:
         self.url = url
         self.description = description
         self.rt = rt
+
+
+class Article:
+    all_articles = []
+
+    def __init__(self, author, title, description, url, urlToImage):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+
+    @classmethod
+    def get_articles(cls, title):
+
+        response = []
+
+        for article in cls.all_articles:
+            if article.source_name == name:
+                response.append(article)
+
+        return response
--- app/models/articles.py
+++ app/models/articles.py
@@ -1,23 +1,3 @@
-class Article:
-    all_articles = []
-
-    def __init__(self, author, title, description, url, urlToImage):
-        self.author = author
-        self.title = title
-        self.description = description
-        self.url = url
-        self.urlToImage = urlToImage
-
-    @classmethod
-    def get_articles(cls, title):
-
-        response = []
-
-        for article in cls.all_articles:
-            if article.source_name == name:
-                response.append(article)
-
-        return response
 
     # def save_article(self):
     #     Article.all_articles.append(self)

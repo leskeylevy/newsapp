e5efcd18fb8a31b01b0aae3aef5f0c0de2221a44
Levy <leskeylevy@gmail.com>
2018-09-03 14:08:18 +0300

    created a main folder inside the app component

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
-from config import DevConfig
+from config import config_options
+
+
 # from flask_bootstrap import Bootstrap
 
 # Initializing application
 def create_app(config_name):
     app = Flask(__name__)
 
-# SETTING UP CONFIGURATION
+    # SETTING UP CONFIGURATION
     app.config.from_object(config_options[config_name])
 
     return app
